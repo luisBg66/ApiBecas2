7b7c67d95ba7be0040d47848fe7e066faebad631
luis <luisbernalgutierrez66@gmail.com>
2024-12-11 00:56:22 -0600

    p2

--- config/cache.php
+++ config/cache.php
@@ -40,9 +40,9 @@ return [
 
         'database' => [
             'driver' => 'database',
-            'connection' => env('DB_CACHE_CONNECTION',null),
+            'connection' => env('DB_CACHE_CONNECTION'),
             'table' => env('DB_CACHE_TABLE', 'cache'),
-            'lock_connection' => env('DB_CACHE_LOCK_CONNECTION',null),
+            'lock_connection' => env('DB_CACHE_LOCK_CONNECTION'),
             'lock_table' => env('DB_CACHE_LOCK_TABLE'),
         ],
 
