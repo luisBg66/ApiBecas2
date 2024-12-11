5dee0626430b0a276803761ea2982a03a07e62da
luis <luisbernalgutierrez66@gmail.com>
2024-12-11 01:02:25 -0600

    pruevacors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['*,https://apibecas2-production.up.railway.app'],
 
     'allowed_origins_patterns' => [],
 
